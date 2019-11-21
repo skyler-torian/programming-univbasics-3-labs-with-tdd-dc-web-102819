923544e807e1b578a3acb66b800415ac40903b14
github username <github email address>
2019-11-21 09:46:09 -0500

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,8 @@
 # Add your variables here
-first_number = 20 
-second_number = 10
+first_number = 5
+second_number = 7
 sum = first_number + second_number
-difference = first_number - second_number 
+difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
+
